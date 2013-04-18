75b5a396ef622fa8cfe30f8977256c643c942133
Frans Slothouber <frans.slothouber@gmail.com>
2013-04-18 08:03:52 +0200

    Using argparse instead of optparse

--- Source/Server/GameEngine/galaxy.py
+++ Source/Server/GameEngine/galaxy.py
@@ -2,16 +2,18 @@
 # vi: spell spl=en
 
 
-from optparse import OptionParser
+import argparse
 
-parser = OptionParser()
-
-parser.add_option("-n", "--name", dest="gamename",
-                  help="Name of the game", metavar="NAME")
-
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
+parser = argparse.ArgumentParser()
+parser.add_argument( '--create', help="Create a new game", metavar="GAME_NAME" )
+parser.add_argument( '--run',    help="Run a turn", metavar="GAME_NAME" )
+parser.add_argument( '--dump',   help="Dump game data", metavar="GAME_NAME" )
+# parser.add_option("-n", "--name", dest="gamename",
+#                   help="Name of the game", metavar="NAME")
+# 
+# parser.add_option("-q", "--quiet",
+#                   action="store_false", dest="verbose", default=True,
+#                   help="don't print status messages to stdout")
 
 #
 # actions 
