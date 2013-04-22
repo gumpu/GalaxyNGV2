009e20f43d71e5e1fabe0e20df58196f6fd127b5
Frans Slothouber <rfsber@xs4all.nl>
2013-04-22 22:20:01 +0200

    Shiptype implementation and unittest

--- Source/Server/GameEngine/elements/shiptype.py
+++ Source/Server/GameEngine/elements/shiptype.py
@@ -2,9 +2,22 @@
 
 class Shiptype(object):
 
-    def __init__( self ):
-        self.unique_key
-        pass
+    def __init__(self, key, name, drive, attacks, weapon, shield, cargo ):
+        self.key     = key
+        self.name    = name
+        self.drive   = drive
+        self.attacks = attacks
+        self.weapon  = weapon
+        self.shield  = shield
+        self.cargo   = cargo
 
-    def key(self):
-        return self.unique_key
+    def mass(self):
+        """Mass of a ship of this type.
+
+        Without any cargo.
+        """
+        m = ( self.drive + self.weapon +
+                0.5*(self.attacks - 1)*self.weapon + 
+                self.shield + self.cargo )
+        return m
+
---
+++ Source/Server/GameEngine/elements/test/test_shiptype.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import unittest
+from elements.shiptype import Shiptype
+
+
+class ShiptypeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.drone     = Shiptype(0,'drone', 1, 0, 0, 0,0)
+        self.flak      = Shiptype(0,'flak', 1, 0, 0, 2, 0)
+        self.destroyer = Shiptype(0,'destroyer', 6, 3, 4, 4, 0)
+        self.freighter = Shiptype(0,'freighter', 30.00, 0, 0.00, 9.50, 10.00)
+
+    def test_mass(self):
+        self.assertAlmostEqual(self.drone.mass(), 1.0)
+        self.assertAlmostEqual(self.flak.mass(), 3.0)
+        self.assertAlmostEqual(self.destroyer.mass(), 18.0)
+        self.assertAlmostEqual(self.freighter.mass(), 49.50)
+
+if __name__=='__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(ShiptypeTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
