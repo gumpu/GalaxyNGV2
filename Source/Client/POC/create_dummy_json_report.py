0e15c138f028fa070165ecc03144bbccf071aae6
Frans Slothouber <rfsber@xs4all.nl>
2013-04-28 17:09:15 +0200

    Dummy json report

---
+++ Source/Client/POC/create_dummy_json_report.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import json
+import random
+
+class Planet( object ):
+    def __init__( self ):
+        self.x = random.randint( 0, 100 )
+        self.y = random.randint( 0, 100 )
+        self.key = '{}{}'.format( self.x, self.y )
+
+class Universe( object ):
+    def __init__( self ):
+        self.size = 100
+        self.planets = {}
+        for i in xrange(10):
+            p = Planet()
+            self.planets[ p.key ] = p
+
+def convert_to_builtin_type( obj ):
+    # Convert objects to a dictionary of their representation
+    d = { '__class__':obj.__class__.__name__ }
+    d.update(obj.__dict__)
+    return d
+
+if __name__=='__main__':
+
+    uni = Universe()
+    print json.dumps( uni, default=convert_to_builtin_type )
+
