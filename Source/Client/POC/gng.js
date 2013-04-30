f2b30f59578f79edab5b2eceec51a87e048077f5
Frans Slothouber <rfsber@xs4all.nl>
2013-04-30 12:25:37 +0200

    Follow mouse position in canvas

--- Source/Client/POC/gng.js
+++ Source/Client/POC/gng.js
@@ -1,22 +1,45 @@
 /* vi: spell spl=en 
  */
 
-
+// Assumes jQuery
+//
 $( document ).ready(function() {
-  console.log( 'ready!' );
+    console.log( 'ready!' );
 
-  var canvas = document.getElementById('map');
-  if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
-        // drawing code here
+    // var canvas = document.getElementById('map');
+    var canvas = $('#map');
+    if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+        // drawing code here
         console.log( 'ready to draw!' );
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
- 
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 55, 50);
+     
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
         ctx.fillRect (30, 30, 55, 50);
-  } else {
-        // canvas-unsupported code here
-  }
+    } else {
+          // canvas-unsupported code here
+    }
+
+    //  var canvas = $('#my_canvas');
+
+    // calculate position of the canvas DOM element on the page
+    var canvasPosition = {
+        x: canvas.offset().left,
+        y: canvas.offset().top
+    };
+
+    canvas.mousemove( function(e) {
+        // use pageX and pageY to get the mouse position
+        // relative to the browser window
+        var mouse = {
+            x: e.pageX - canvasPosition.x,
+            y: e.pageY - canvasPosition.y
+        }
+        console.log( 'click' + mouse.x + ',' + mouse.y );
+        // now you have local coordinates,
+        // which consider a (0,0) origin at the
+        // top-left of canvas element
+    });
 });
 
